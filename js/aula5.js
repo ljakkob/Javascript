8240506571bcb62a1d46a412a6b4b1b14e049ebb
ljakkob <larissa.jacob01@gmail.com>
2017-07-21 21:58:03 -0300

    Dia 01
    eventods / variáveis / constantes e camadas

---
+++ js/aula5.js
@@ -0,0 +1,20 @@
+/*
+
+	função : ligar ()
+	
+
+*/
+
+
+
+
+
+
+function ligar(){
+
+
+		var nome = prompt("Qual o seu nome?");
+		alert("Prazer em conhecer vc "+nome);
+
+		document.getElementById("xti").src="ligada.jpg";
+	}
